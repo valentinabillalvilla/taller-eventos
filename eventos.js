2054eaf5bfbcb38088adf8cc24bc9aaa2585c67e
valentinabillalvilla <valentinabillalvilla@gmail.com>
2024-09-02 22:14:35 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     
     let miDiv = document.querySelector('div');
+    let miBoton = document.getElementById('saludar-btn');
+
     miDiv.addEventListener('click', function () {
         alert('¡Hola! Soy el div');
     });
+
+    miBoton.addEventListener('click', function (event) {
+        alert('¡Hola!');
+        event.stopPropagation();
+    });
+
 });
