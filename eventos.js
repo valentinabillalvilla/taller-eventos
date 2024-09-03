8af79135a823a8823a835a48da45b337c076f610
valentinabillalvilla <valentinabillalvilla@gmail.com>
2024-09-02 21:39:31 -0300

    eventos.js

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    let miDiv = document.querySelector('div');
+    miDiv.addEventListener('click', function () {
+        alert('¡Hola! Soy el div');
+    });
+});
